b3fa1264d4a74160145fadd8d36b4cb055e9edce
VictorPineiroHernandez <164346631+VictorPineiroHernandez@users.noreply.github.com>
2024-05-20 10:17:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Devil may cry es una franquicia de videojuegos hecha por CapCom, también conocida por otras franquicias famosas como Resident Evil o Street fighter');
+});
